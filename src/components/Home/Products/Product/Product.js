5f76a6574f590b03cabcdeedbeafa8ba852c7c07
RicciWoo <ricciwoo@gmail.com>
2020-02-13 11:30:34 +0800

    update data-item-url

--- src/components/Home/Products/Product/Product.js
+++ src/components/Home/Products/Product/Product.js
@@ -16,7 +16,7 @@ const Product = ({ product }) => {
               data-item-name={product.title}
               data-item-price={product.price}
               data-item-image={product.image.fluid.src}
-              data-item-url="gatsby-coffee-udemy.netlify.com"
+              data-item-url="/"
             >
               Add to Cart
             </button>
